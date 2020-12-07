fd4856b39557ed238e92a14e7e927a900aad66f3
Muratcan Akçay <muratcanakcay@gmail.com>
2020-12-07 23:39:53 +0100

    Update pct_stage4.c

--- Lab_03/pct_stage4.c
+++ Lab_03/pct_stage4.c
@@ -34,18 +34,18 @@ void* work(void *voidArgs);
 
 int main(int argc, char** argv) 
 {
-	srand(time(NULL));
+    srand(time(NULL));
     int n, m, k;
     
-	readArguments(argc, argv, &n, &m, &k);
+    readArguments(argc, argv, &n, &m, &k);
 		
     // allocate memory for matrix    
     char** matrix = (char**)malloc(sizeof(char*) * n);
     if (matrix == NULL) ERR("Malloc error for matrix!");
     for(int r = 0; r < n; r++)
     {
-            matrix[r] = (char*)malloc(sizeof(char) * m);
-            if (matrix[r] == NULL) ERR("Malloc error for matrix!");
+        matrix[r] = (char*)malloc(sizeof(char) * m);
+        if (matrix[r] == NULL) ERR("Malloc error for matrix!");
     }
 
     // fill in matrix with random characters
@@ -77,7 +77,7 @@ int main(int argc, char** argv)
     // prepare threads array
     for (int i=0; i < k; i++) 
     {
-		threads[i].seed = rand();
+        threads[i].seed = rand();
         threads[i].n = n;
         threads[i].m = m;
         threads[i].matrix = matrix;
@@ -87,7 +87,7 @@ int main(int argc, char** argv)
     
 	// create threads
     for (int i = 0; i < k; i++)
-	 	if(pthread_create(&threads[i].tid, NULL, work, &threads[i])) ERR("Failed to create student thread!");
+	    if(pthread_create(&threads[i].tid, NULL, work, &threads[i])) ERR("Failed to create student thread!");
 	
     // get input from stdin    
     while(true)
@@ -103,14 +103,15 @@ int main(int argc, char** argv)
         {
             for (int r = 0; r < n; r++) 
             {
-                {
-                    for (int c = 0; c < m; c++) 
-                    printf(" %c", matrix[r][c]);            
-                }
+                for (int c = 0; c < m; c++) 
+                    printf(" %c", matrix[r][c]);                            
+                
                 printf("\n");                
             }
+            
             fflush(stdin);
         }
+
         if(strcmp(buf, "replace") == 0)
         {
             for (int i = 0; i < num; i++) 
@@ -118,19 +119,22 @@ int main(int argc, char** argv)
                 printf("Sending USR1\n");
                 kill(0, SIGUSR1);
             }
+
             fflush(stdin);
         }
+        
         if(strcmp(buf, "exit") == 0)
         {
             for(int i=0; i < k; i++)
                 pthread_kill(threads[i].tid, SIGINT);
+            
             break;
         }
     }	
 	
 	// join with terminated threads
     for (int i = 0; i < k; i++) 
-	 	if(pthread_join(threads[i].tid, NULL)) ERR("Failed to join with a thread!");
+        if(pthread_join(threads[i].tid, NULL)) ERR("Failed to join with a thread!");
 	
 	exit(EXIT_SUCCESS);
 }
@@ -146,7 +150,7 @@ void readArguments(int argc, char** argv, int* n, int* m, int* k)
 
 	if (argc == 4) 
 	{
-		*n = atoi(argv[1]);
+        *n = atoi(argv[1]);
         *m = atoi(argv[2]);
         *k = atoi(argv[3]);
          
@@ -173,25 +177,25 @@ void* work(void *voidArgs)
         sigwait(args->mask, &sigNo);
         
         switch (sigNo) 
-		{
-			case SIGUSR1:
+        {
+            case SIGUSR1:
                 rr = rand_r(&args->seed) % args->n;
                 rc = rand_r(&args->seed) % args->m;
-				
+
                 pthread_mutex_lock(&args->mxRows[rr]);
                 int i =  rand_r(&args->seed);
                 char newChar = 'a'+(i%('z'-'a'+1)); 
                 printf("[%d] received SIGUSR1: Replacing '%c' to '%c' at index [%d, %d]\n", (int)args->tid, (args->matrix[rr][rc]), newChar, rr, rc);
                 args->matrix[rr][rc] = newChar; 
-				pthread_mutex_unlock(&args->mxRows[rr]);
+                pthread_mutex_unlock(&args->mxRows[rr]);
                 
                 break;
-			case SIGINT:				
-				printf("[%d] received SIGINT: Exiting\n", (int)args->tid);
+            case SIGINT:				
+                printf("[%d] received SIGINT: Exiting\n", (int)args->tid);
                 return NULL;
-			default:
-				printf("unexpected signal %d\n", sigNo);
-				exit(1);
+            default:
+                printf("unexpected signal %d\n", sigNo);
+                exit(1);
 		}
     }
     
